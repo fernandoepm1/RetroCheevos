c35ff7886dbd3cdcd1470271eb936ec054e51bf0
Fernando Eufrásio <fernando.epm1@gmail.com>
2025-06-25 00:06:26 -0300

    feat(sets): add the initial lookups

---
+++ src/ps1/woody-woodpecker-racing/data/lookups.ts
@@ -0,0 +1,37 @@
+export const modeLookup = {
+  1: 'Quest Mode',
+  2: 'World Championship',
+  3: 'Time Trial',
+  4: '1P/2P Race'
+}
+
+export const characterLookup = {
+  0: 'Woody',
+  1: 'Wally Walrus',
+  2: 'Buzz Buzzard',
+  3: 'Winnie',
+  4: 'Chilly Willy',
+  5: 'Knothead',
+  6: 'Ms. Meany',
+  7: 'Denver',
+  8: 'Spacemouse',
+}
+
+export const trackLookup = {
+  0: 'Brazil',
+  1: 'Woody Glen',
+  2: 'Death Valley',
+  3: 'Quarry',
+  4: 'Italy',
+  5: "Meany's Motorsports",
+  6: 'Sawmill',
+  7: 'Junkyard',
+  8: 'Japan',
+  9: 'Chilly International',
+  10: 'Pebble Bay',
+  11: 'Barnyard',
+  12: 'Loch Ness',
+  13: 'Steelworks',
+  14: 'Space Speedway',
+  15: 'Vegas Runway',
+};
